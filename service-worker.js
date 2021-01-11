00dc0810d8a2185c7222cb09e743867ea93c874b
Peja <peja@relab.cc>
2021-01-11 16:01:54 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a089c41bf08e49bfbb2ed5833d9d434c.js"
+  "/precache-manifest.3b0cdecb5ddf0a001073a1683c5cf3fd.js"
 );
 
 self.addEventListener('message', (event) => {
